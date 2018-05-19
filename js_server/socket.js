f6a989743be4363a812e9938ab20227de47cde70
hi_melnikov <melnikovam10@gmail.com>
2018-05-19 20:15:01 +0300

    made users

--- js_server/socket.js
+++ js_server/socket.js
@@ -7,16 +7,24 @@ var io = require('socket.io')(server);
 server.listen(3000);
 
 
-var game = [[0,0,0]]
+var game = [[0,0,0]];
+var users= [];
+var new_id = 10;
+
 app.get('/', function(request, response){
 
   response.sendFile(__dirname + "/index.html");
-
+  let id = -1;
+  if (new_id<=20){
+    id = new_id;
+    new_id +=1;
+    users.push({id: id, cookie : request.headers.cookie, lastButtons: ''});
+  }
 });
 
 io.on('connection', function(socket){
-  socket.on('user.pushButton', function(data){
-
+  console.log(socket.request.headers.cookie);
+  socket.on('user.pushButton', function(button){
   });
 });
 
